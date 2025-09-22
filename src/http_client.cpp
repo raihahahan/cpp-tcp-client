ef64fc7d7ca26c3f1264f1cbdac911b1ee898fe6
raihahahan <mraihandev@gmail.com>
2025-09-22 12:18:17 +0800

    move business logic out of http client

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -1,21 +1,7 @@
 #include "http_client.h"
 #include "tcp_socket.h"
 #include "tls_client.h"
-#include <regex>
 #include <iostream>
-
-std::string parse(std::string& response) {
-    auto pos = response.find("\r\n\r\n");
-    response = (pos != std::string::npos) ? response.substr(pos + 4) : response;
-    std::regex ip_regex(R"((\d{1,3}\.){3}\d{1,3})");
-    std::smatch match;
-    if (std::regex_search(response, match, ip_regex)) {
-        return match.str();
-    } else {
-        std::cerr << "Could not parse regex\n";
-        return "";
-    }
-}
 
 HttpClient::HttpClient(const std::string& host, const uint16_t& port) : host{std::move(host)}, port{port} {};
 
@@ -30,7 +16,6 @@ std::string HttpClient::get(const std::string& path) {
 
     tlsClient.write(request);
     std::string response = tlsClient.read_all();
-    response = parse(response);
     tlsClient.shutdown();
 
     return response;
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "http_client.h"
 #include <iostream>
+#include <regex>
+
+std::string parse(std::string& response) {
+    auto pos = response.find("\r\n\r\n");
+    response = (pos != std::string::npos) ? response.substr(pos + 4) : response;
+    std::regex ip_regex(R"((\d{1,3}\.){3}\d{1,3})");
+    std::smatch match;
+    if (std::regex_search(response, match, ip_regex)) {
+        return match.str();
+    } else {
+        std::cerr << "Could not parse regex\n";
+        return "";
+    }
+}
 
 int main() {
     constexpr const char* HOST = "varlabs.comp.nus.edu.sg";
@@ -7,6 +21,7 @@ int main() {
     constexpr uint16_t PORT = 443;
     HttpClient client(HOST, PORT);
     std::string response = client.get(PATH);
+    response = parse(response);
     std::string result = "My public IP address is " + response;
     std::cout << result << std::endl;
     return 0;
